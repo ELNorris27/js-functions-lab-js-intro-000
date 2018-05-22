5d80f134da6f6f8c32885fe9cb4c318d553da833
Elizabeth Norris <elnorris27@gmail.com>
2018-05-22 03:47:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var happyHolidays = function() {
-  return "Happy holidays";
+  return "Happy holidays!";
 };
 
 var happyHolidaysTo = function(name) {
   return `Happy Holidays, ${name}!`;
 };
 
+
 var happyHolidayTo = function(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
+
 var holidayCountdown = function(days, holiday) {
-  return `It's ${days} days until ${holiday}!`;
+  return `It\'s ${days} days until ${holiday}!`;
 }
+holidayCountdown(20, "Mother's Day");
